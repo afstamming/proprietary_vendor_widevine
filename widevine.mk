71f0a128b575456fca5e55e8601d89838d40326f
anotherbloke <bloke2.null@null.null>
2019-10-20 12:24:20 +0200

    widevine: Convert to Android.bp with vintf fragment.

--- widevine.mk
+++ widevine.mk
@@ -3,7 +3,4 @@ WV_PATH := vendor/widevine
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(WV_PATH)/sepolicy/vendor
 
-PRODUCT_COPY_FILES += \
-    $(WV_PATH)/vendor/bin/hw/android.hardware.drm@1.1-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.1-service.widevine \
-    $(WV_PATH)/vendor/etc/init/android.hardware.drm@1.1-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.1-service.widevine.rc \
-    $(WV_PATH)/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so
+PRODUCT_PACKAGES += android.hardware.drm@1.1-service.widevine
