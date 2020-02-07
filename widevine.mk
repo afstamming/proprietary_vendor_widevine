dc4ce6ddc7f584270c14f2894fabfb5fcbd4e86c
somebloke <dev.null@null.null>
2020-02-07 18:38:43 +0100

    [16.0] Uprev to 1.2 and 64bit, taken from sargo dump

--- widevine.mk
+++ widevine.mk
@@ -4,6 +4,6 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     $(WV_PATH)/sepolicy/vendor
 
 PRODUCT_COPY_FILES += \
-    $(WV_PATH)/vendor/bin/hw/android.hardware.drm@1.1-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.1-service.widevine \
-    $(WV_PATH)/vendor/etc/init/android.hardware.drm@1.1-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.1-service.widevine.rc \
-    $(WV_PATH)/vendor/lib/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib/libwvhidl.so
+    $(WV_PATH)/vendor/bin/hw/android.hardware.drm@1.2-service.widevine:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.drm@1.2-service.widevine \
+    $(WV_PATH)/vendor/etc/init/android.hardware.drm@1.2-service.widevine.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.drm@1.2-service.widevine.rc \
+    $(WV_PATH)/vendor/lib64/libwvhidl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libwvhidl.so
