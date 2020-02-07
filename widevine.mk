7ad4008b244fe4602a20cabcfd358fe1fdfb213a
somebloke <dev.null@null.null>
2020-02-07 15:59:29 +0100

    Uprev to 1.2 and 64bit, taken from sargo dump

--- widevine.mk
+++ widevine.mk
@@ -3,4 +3,4 @@ WV_PATH := vendor/widevine
 BOARD_VENDOR_SEPOLICY_DIRS += \
     $(WV_PATH)/sepolicy/vendor
 
-PRODUCT_PACKAGES += android.hardware.drm@1.1-service.widevine
+PRODUCT_PACKAGES += android.hardware.drm@1.2-service.widevine
